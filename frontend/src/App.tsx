28d2d62bb32503c0a6272fe29e1af90be1f9abdd
Ayush Pandey <ayushpandey5@icloud.com>
2024-04-25 19:30:42 +1000

    blog page adde

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
+import { Blogs } from './pages/Blogs'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/signin' element={<Signin/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/blogs' element={<Blogs/>}/>
       </Routes>
     </BrowserRouter>
     </>
